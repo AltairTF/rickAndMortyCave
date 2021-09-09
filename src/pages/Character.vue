aa9453f5df55b7acbc5c03933338f111e92cf66a
AltairTF <todescatto6@gmail.com>
2021-09-09 11:52:21 -0300

    characters and styling

--- src/pages/Character.vue
+++ src/pages/Character.vue
@@ -1,88 +1,47 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-card class="my-card" flat bordered>
-      <q-card-section>
-        <q-card-section class="my-info-card">
-          <h4 class="my-font">{{ characterInfo.name }}</h4>
-          <q-img
-            class="my-character-image"
-            :src="characterInfo.image"
-            spinner-color="white"
-          />
-          <div class="row my-character-card">
-            <p class="col-4 text-subtitle2">Species</p>
-            <p class="col-8 text-body2 text-right">
-              {{ characterInfo.species }}
-            </p>
-          </div>
-          <div class="row my-character-card">
-            <p class="col-4 text-subtitle2">Type</p>
-            <p class="col-8 text-body2 text-right">
-              {{ characterInfo.type }}
-            </p>
-          </div>
-          <div class="row my-character-card">
-            <p class="col-4 text-subtitle2">Gender</p>
-            <p class="col-8 text-body2 text-right">
-              {{ characterInfo.gender }}
-            </p>
-          </div>
-          <div class="row my-character-card">
-            <p class="col-4 text-subtitle2">Origin</p>
-            <div class="col-8 text-body2 text-right">
-              <div>
-                <div class="row">
-                  <p class="col-4 text-subtitle2">Name</p>
-                  <p class="col-8 text-body2 text-right">
-                    {{ origin.name }}
-                  </p>
-                </div>
-                <div class="row">
-                  <p class="col-4 text-subtitle2">Type</p>
-                  <p class="col-8 text-body2 text-right">
-                    {{ origin.type }}
-                  </p>
-                </div>
-                <div class="row">
-                  <p class="col-4 text-subtitle2">Dimension</p>
-                  <p class="col-8 text-body2 text-right">
-                    {{ origin.dimension }}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="row my-character-card" style="border: none">
-            <p class="col-4 text-subtitle2">Location</p>
-            <div class="col-8 text-body2 text-right">
-              <div>
-                <div class="row">
-                  <p class="col-4 text-subtitle2">Name</p>
-                  <p class="col-8 text-body2 text-right">
-                    {{ location.name }}
-                  </p>
-                </div>
-                <div class="row">
-                  <p class="col-4 text-subtitle2">Type</p>
-                  <p class="col-8 text-body2 text-right">
-                    {{ location.type }}
-                  </p>
-                </div>
-                <div class="row">
-                  <p class="col-4 text-subtitle2">Dimension</p>
-                  <p class="col-8 text-body2 text-right">
-                    {{ location.dimension }}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </q-card-section>
-
-        <q-list dense padding class="rounded-borders">
-          <p class="text-subtitle2 q-pa-sm text-center">
-            Episodes {{ characterInfo.name }} is in
+  <q-page class="flex flex-center" v-if="characterInfo.name">
+    <div class="row" bordered>
+      <div class="col my-info-card">
+        <q-img
+          class="my-character-image"
+          :src="characterInfo.image"
+          spinner-color="white"
+        />
+        <p class="text-subtitle2">{{ characterInfo.name }}</p>
+        <div class="row">
+          <p class="col-6 text-subtitle2">Species</p>
+          <p class="col-6 text-body2">
+            {{ characterInfo.species }}
           </p>
+        </div>
+        <div class="row" v-if="characterInfo.type">
+          <p class="col-6 text-subtitle2">Type</p>
+          <p class="col-6 text-body2">
+            {{ characterInfo.type }}
+          </p>
+        </div>
+        <div class="row">
+          <p class="col-6 text-subtitle2">Gender</p>
+          <p class="col-6 text-body2">
+            {{ characterInfo.gender }}
+          </p>
+        </div>
+        <div class="row">
+          <p class="col-6 text-subtitle2">Origin</p>
+          <p class="col-6 text-body2">
+            {{ origin.name }}
+          </p>
+        </div>
+        <div class="row">
+          <p class="col-6 text-subtitle2">Location</p>
+          <p class="col-6 text-body2">
+            {{ location.name }}
+          </p>
+        </div>
+      </div>
+      <div class="col my-info-card">
+        <q-list dense padding class="rounded-borders">
+          <p class="text-subtitle2">Episodes {{ characterInfo.name }} is in</p>
           <q-item
             clickable
             v-ripple
@@ -90,12 +49,14 @@
             :key="x"
             @click="retrieving(x.id)"
           >
-            <q-item-section>{{ x.episode }}</q-item-section>
+            <q-item-section class="text-subtitle2">{{
+              x.episode
+            }}</q-item-section>
             <q-item-section>{{ x.name }}</q-item-section>
           </q-item>
         </q-list>
-      </q-card-section>
-    </q-card>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -103,6 +64,7 @@
 import axios from "axios";
 import { defineComponent } from "vue";
 import { SessionStorage } from "quasar";
+import { Loading } from "quasar";
 
 export default defineComponent({
   name: "Character",
@@ -137,6 +99,10 @@ export default defineComponent({
     if (SessionStorage.id == this.id) {
       this.id = SessionStorage.getItem("id");
     }
+    Loading.show({
+      delay: 500,
+      message: "Please wait...",
+    });
     axios
       .post(
         "https://rickandmortyapi.com/graphql",
@@ -177,6 +143,7 @@ export default defineComponent({
         }
       )
       .then((res) => {
+        Loading.hide();
         this.characterInfo = res.data.data.character;
         this.origin = res.data.data.character.origin;
         this.location = res.data.data.character.location;
@@ -191,22 +158,19 @@ export default defineComponent({
 
 <style lang="sass" scoped>
 .my-info-card
-  background-color: $info
-  border-radius: 3%
-.my-card
-  max-width: 80vh
-  background-color: $primary
-  width: 75%
-  margin: 2vh 0 2vh
-.my-character-card
-  padding: 2vh 0 0 0
-  border-bottom: 1px solid $dark
+  padding: 1vh
+  text-align: center
+  margin: 1vh
+  background: linear-gradient(45deg, $secondary, $primary )
+  border-radius: 10px
+  border: 2px solid $dark
+  height: 100%
 .my-character-image
   margin: 0 0 2vh 0
   border: 2px solid $dark
-  border-radius: 3%
-  height: 30%
-  max-width: 30%
+  border-radius: 50%
+  height: 40%
+  max-width: 40%
 .my-font
   font-family: 'customfont'
   color: $primary
